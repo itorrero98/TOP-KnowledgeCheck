9f590414af8189eb14957686b7fb1e16eb67e939
itorr <itorrero98@gmail.com>
2022-02-11 20:41:23 -0700

    finish knowledge check for foundations flexbox

---
+++ Knowledge.js
@@ -0,0 +1,17 @@
+/**
+ * The odin project knowledge check questions
+ * This is where I will log the different questions asked in the Odin project and my answers
+ */
+
+//*****************FOUNDATIONS BEGINNING******************
+/** FLEXBOX
+ * Section - Alignment
+ * * 1. What's the difference between justify-content and align-items?
+ * * * Justify-content aligns items along the primary axis, in normal flex direction it align's items horizontally and in flex-direction column it aligns vertically
+ Align-items does the opposite, vertical for normal flex direction and horizontal for column direction
+ * * 2. How do you use flexbox to completely center a div inside a flex container?
+ * * * If the div is aligning itself: align-self: center; justify-self: center. Otherwise if the container is centering: justify-content: center; align-items:center
+ * * 3. What's the difference between justify-content: space-between and justify-content: space-around?
+ * * * Space-between evenly spaces the items in a container with even space between elements in the container whereas space-around adds even spacing on either
+ side of the main axis but the space between isnt' necessarily even
+ */
